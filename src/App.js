d7c895655f8a166bda63756bb8f57a0e00e038eb
Christian Sihotang <christian.sihotang23@gmail.com>
2021-10-05 23:23:54 +0700

    Done

--- src/App.js
+++ src/App.js
@@ -1,25 +1,237 @@
+/* eslint-disable react/jsx-props-no-spreading */
+/* eslint-disable consistent-return */
+/* eslint-disable space-before-function-paren */
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { styled } from '@mui/material/styles';
+import { Form, Field } from 'react-final-form';
+import {
+  AppBar,
+  Box,
+  Toolbar,
+  Typography,
+  IconButton,
+  Container,
+  TextField,
+  Button,
+} from '@mui/material';
+import { FORM_ERROR } from 'final-form';
+import LogoIst from 'assets/ist.png';
+import infaq from 'assets/logo.png';
 
 function App() {
+  const InputContainer = styled('div')(() => ({
+    display: 'flex',
+    justifyContent: 'center',
+    padding: '0px 20px',
+  }));
+
+  const ButtonContainer = styled('div')(() => ({
+    display: 'flex',
+    justifyContent: 'center',
+  }));
+  const CustomTextField = styled(TextField)(() => ({
+    minWidth: '200px',
+    margin: '10px',
+    '&.full': {
+      minWidth: '420px',
+    },
+  }));
+
+  const sleep = (ms) => new Promise((resolve) => setTimeout(resolve, ms));
+
+  const onSubmit = async (values) => {
+    await sleep(300);
+    window.alert(JSON.stringify(values));
+    if (values.username !== 'erikras') {
+      return { username: 'Unknown username' };
+    }
+    if (values.password !== 'finalformrocks') {
+      return { [FORM_ERROR]: 'Login Failed' };
+    }
+    window.alert('LOGIN SUCCESS!');
+  };
+
+  const formatAmountIDR = (input = '') => {
+    if (!input) return input;
+    const inputNumber = input.replace(/,/g, '').replace(/[^0-9]/g, '');
+
+    if (Number(inputNumber) === 0) return '';
+    if (inputNumber[0] === '0' || inputNumber[3] === '0')
+      return `Rp ${Number(inputNumber)
+        .toString()
+        .replace(/,/g, '')
+        .replace(/[^0-9]/g, '')
+        .replace(/\B(?=(\d{3})+(?!\d))/g, ',')}`;
+    // eslint-disable-next-line consistent-return
+    if (inputNumber.length >= 43)
+      return `Rp ${inputNumber
+        .slice(0, -1)
+        .replace(/,/g, '')
+        .replace(/[^0-9]/g, '')
+        .replace(/\B(?=(\d{3})+(?!\d))/g, ',')}`;
+    // eslint-disable-next-line consistent-return
+    return `Rp ${inputNumber
+      .replace(/,/g, '')
+      .replace(/[^0-9]/g, '')
+      .replace(/\B(?=(\d{3})+(?!\d))/g, ',')}`;
+  };
+
   return (
-    <div className='App'>
-      <header className='App-header'>
-        <img src={logo} className='App-logo' alt='logo' />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className='App-link'
-          href='https://reactjs.org'
-          target='_blank'
-          rel='noopener noreferrer'
+    <Box sx={{ flexGrow: 1 }}>
+      <AppBar
+        position='static'
+        sx={{
+          backgroundColor: 'white',
+          boxShadow: '0px 3px 10px rgba(188, 200, 231, 0.2)',
+          paddingLeft: '16px',
+          paddingRight: '56px',
+        }}
+        elevation={0}
+      >
+        <Toolbar variant='dense'>
+          <IconButton
+            edge='start'
+            color='inherit'
+            aria-label='menu'
+            sx={{ mr: 2 }}
+          >
+            <img
+              src={LogoIst}
+              alt='logo-ist'
+              style={{ width: 'auto', height: '50px' }}
+            />
+          </IconButton>
+          <Typography variant='h6' color='black' component='div'>
+            Donasi
+          </Typography>
+        </Toolbar>
+      </AppBar>
+      <Container
+        sx={{
+          backgroundColor: '#F4F7FB',
+          minHeight: '100vh',
+          minWidth: '100vw',
+          display: 'flex',
+          justifyContent: 'start',
+          alignContent: 'center',
+          paddingTop: '64px',
+          paddingBottom: '30px',
+          backgroundImage: `url(${infaq})`,
+          backgroundRepeat: 'no-repeat',
+          backgroundPositionX: 'right',
+          backgroundPositionY: '100px',
+        }}
+      >
+        <Box
+          margin='20px 40px'
+          padding='40px'
+          minWidth='500px'
+          maxHeight='420px'
+          borderRadius='25px'
+          bgcolor='white'
+          display='flex'
+          flexDirection='column'
+          justifyContent='start'
+          alignContent='center'
         >
-          Learn React
-        </a>
-      </header>
-    </div>
+          <Typography
+            sx={{ marginBottom: '20px' }}
+            variant='h5'
+            color='black'
+            component='div'
+          >
+            Mari Donasi
+          </Typography>
+          <Form
+            onSubmit={onSubmit}
+            validate={(values) => {
+              const errors = {};
+              if (!values.email) {
+                errors.email = 'Email tidak boleh kosong';
+              } else if (
+                !/^[a-z0-9](\.?[a-z0-9]){5,}@g(oogle)?mail\.com$/.test(
+                  values.email
+                )
+              ) {
+                errors.email = 'Format email salah dan hanya menerima gmail';
+              }
+              if (!values.password) {
+                errors.password = 'Password tidak boleh kosong';
+              }
+              if (!values.donasi) {
+                errors.donasi = 'Donasi tidak boleh kosong';
+              }
+              return errors;
+            }}
+            render={({ handleSubmit }) => (
+              <form onSubmit={handleSubmit}>
+                <InputContainer>
+                  <Field name='email'>
+                    {({ input, meta }) => (
+                      <CustomTextField
+                        id='outlined-email-input'
+                        label='Email'
+                        type='text'
+                        className='full'
+                        placeholder='Masukkan Email Kamu'
+                        autoComplete='current-email'
+                        error={meta.error && meta.touched}
+                        helperText={meta.error && meta.touched && meta.error}
+                        {...input}
+                      />
+                    )}
+                  </Field>
+                </InputContainer>
+                <InputContainer>
+                  <Field name='password'>
+                    {({ input, meta }) => (
+                      <CustomTextField
+                        id='outlined-password-input'
+                        label='Password'
+                        type='password'
+                        className='full'
+                        placeholder='Masukkan Password Kamu'
+                        autoComplete='current-password'
+                        error={meta.error && meta.touched}
+                        helperText={meta.error && meta.touched && meta.error}
+                        {...input}
+                      />
+                    )}
+                  </Field>
+                </InputContainer>
+
+                <InputContainer>
+                  <Field name='donasi' parse={formatAmountIDR}>
+                    {({ input, meta }) => (
+                      <CustomTextField
+                        id='outlined-donasi-input'
+                        label='Donasi'
+                        type='text'
+                        className='full'
+                        placeholder='Masukkan Donasi Kamu'
+                        autoComplete='current-donasi'
+                        error={meta.error && meta.touched}
+                        helperText={meta.error && meta.touched && meta.error}
+                        {...input}
+                      />
+                    )}
+                  </Field>
+                </InputContainer>
+                <ButtonContainer>
+                  <Button
+                    type='submit'
+                    sx={{ width: '420px', height: '46px', margin: '10px' }}
+                    variant='contained'
+                  >
+                    Submit
+                  </Button>
+                </ButtonContainer>
+              </form>
+            )}
+          />
+        </Box>
+      </Container>
+    </Box>
   );
 }
 
